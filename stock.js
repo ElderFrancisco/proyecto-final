4d953e9a76a3411450f50d17697f26d271a1af37
Francisco <franciscoelder0@gmail.com>
2023-02-08 15:23:29 -0300

    first commit

---
+++ stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+    {id: 1, nombre: "Whey Protein", tipo: "proteina", cantidad: 1, desc: "proteina en polvo ENA SPORT", precio: 7200, sabor: "vainilla", img: './img/proteinaena.webp'},
+    {id: 2, nombre: "Whey Protein", tipo: "proteina", cantidad: 1, desc: "proteina en polvo ENA SPORT", precio: 7200, sabor: "frutilla", img: './img/proteinaena2.webp'},
+    {id: 3, nombre: "Whey Protein", tipo: "proteina", cantidad: 1, desc: "proteina en polvo ENA SPORT", precio: 7200, sabor: "chocolate", img: './img/proteinaena3.webp'},
+    {id: 4, nombre: "Whey Protein", tipo: "proteina", cantidad: 1, desc: "proteina en polvo ENA SPORT", precio: 7200, sabor: "cookies and cream", img: './img/proteinaena4.webp'},
+    
+    {id: 5, nombre: "PUMP V8", tipo: "preentreno", cantidad: 1, desc: "Pre entreno en polvo para potenciar tus entrenamientos", precio: 4000, sabor: "Citrus slush", img: './img/preentreno1.webp'},
+    {id: 6, nombre: "PUMP V8", tipo: "preentreno", cantidad: 1, desc: "Pre entreno en polvo para potenciar tus entrenamientos", precio: 4000, sabor: "Grape attack", img: './img/preentreno2.webp'},
+    {id: 7, nombre: "PUMP V8", tipo: "preentreno", cantidad: 1, desc: "Pre entreno en polvo para potenciar tus entrenamientos", precio: 4000, sabor: "Açai power", img: './img/preentreno3.webp'},
+    {id: 8, nombre: "PUMP V8", tipo: "preentreno", cantidad: 1, desc: "Pre entreno en polvo para potenciar tus entrenamientos", precio: 4000, sabor: "Watermelon", img: './img/preentreno4.webp'},
+    
+    {id: 9, nombre: "Creatine Monohydrate", tipo: "creatina", cantidad: 1, desc: "Creatina monohidratada para aumentar fuerza y masa muscular", precio: 1100, sabor: "ninguno", img: './img/creatina1.webp'},
+    {id: 10, nombre: "Creatina Micronizada", tipo: "creatina", cantidad: 1, desc: "Creatina micronizada para aumentar fuerza y masa muscular", precio: 9200, sabor: "ninguno", img: './img/creatina2.webp'},
+    {id: 11, nombre: "Creatina Classic", tipo: "creatina", cantidad: 1, desc: "¡¡por el precio de 1 te llevas 2!!", precio: 18200, sabor: "ninguno", img: './img/creatina3.webp'},
+]
